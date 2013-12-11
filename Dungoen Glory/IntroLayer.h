d829312583a5f018fc40b7d6a6832bffb93c9cbc
Isaac <anglican333@yahoo.com.au>
2013-12-11 14:24:53 +1100

    Initial Commit

---
+++ Dungoen Glory/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  Dungoen Glory
+//
+//  Created by Anglican333 on 11/12/13.
+//  Copyright Abimon Inc 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
